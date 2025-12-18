161332ae6c90b9469ac1ac4266cdc9f297dd9643
pornpimon21 <sommana646@gmail.com>
2025-12-18 19:57:26 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1277,7 +1277,7 @@ if (matchedMajor) {
     hero: {
       type: "image",
       url: safeText(matchedMajor?.logoUrl) || "https://www.uru.ac.th/images/logouru2011.png",
-      size: "medium",
+      size: "kilo",
       aspectRatio: "1.51:1",
       aspectMode: "fit"
     },
@@ -1478,7 +1478,7 @@ const majorName = rec.major || "";
     hero: {
       type: "image",
       url: rec.logoUrl || "https://www.uru.ac.th/images/logouru2011.png",
-      size: "full",
+      size: "kilo",
       aspectRatio: "1.51:1",
       aspectMode: "fit"
     },
